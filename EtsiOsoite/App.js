4649e9f12ec2387562cc0737392f5d61d451628f
putusel <elina.putus@gmail.com>
2022-09-23 08:18:37 +0300

    Ei toimi vielä

--- EtsiOsoite/App.js
+++ EtsiOsoite/App.js
@@ -1,58 +1,55 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, StatusBar, View, Button, TextInput } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, TextInput, Button, Dimensions } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
 
 export default function App() {
-  const [text, setText] = useState('');
-  const [location, setLocation] = useState(null); // State where location is saved'
-  const [repositories, setRepositories] = useState({});
-  const [coordinates, setCoordinates] = useState({latitude: 60.200692, longitude: 24.934302, latitudeDelta: 0.0322, longitudeDelta: 0.0221});
+
+  const [location, setLocation] = useState('');
+  const [coordinates, setCoordinates] = useState({ latitude: 60.200692, longitude: 24.934302, latitudeDelta: 0.0322, longitudeDelta: 0.0221});
 
   const apikey = '9CZ5yt0C4TcCgBMqY6HffGPrdansAJrG';
+  const url = 'http://www.mapquestapi.com/geocoding/v1/address?'
+ 
+  const getLocation = async () => {
+    try{
+      const response = await fetch(`${url}key=${apikey}&location=${location}`);
+      const data = await response.json();
 
+      let coordinates = {
+        latitude: data.results[0].locations[0].latLng.lat,
+        longitude: data.results[0].locations[0].latLng.lng,
+        latitudeDelta: 0.0322, /*starting point Haaga-Helia*/
+        longitudeDelta: 0.0221} /*starting point Haaga-Helia*/
   
-  const getLocation = () => {  
-    fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=${apikey}&location=${text}`)  
-    .then(response => response.json())  
-    .then(data => setRepositories(data.results.locations.latLng))
-    let coordinate = 
-    {latitude: results[0].locations[0].latLng.lat,
-    longitude: results[0].locations[0].latLng.lng, 
-    latitudeDelta: 0.0322, 
-    longitudeDelta: 0.0221}
-    setCoordinates(coordinate)
-    .catch(error => {         
-          Alert.alert('Error', error);   
-    });
-  }
+      setCoordinates(coordinates)
 
-  /*useEffect(() => { getLocation(); }, [])*/
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        region={coordinates}
-      >
+      <MapView  style={styles.map}   
+        region={coordinates}>
         <Marker
           coordinate={coordinates}
-          title='Haaga-Helia'
-        />
-      </MapView>
-      <TextInput 
-          style={styles.input} 
-          placeholder="enter a location"
-          onChangeText={text => setText(text)} value={text}
-        />
-      <Button onPress={getLocation}title="Show"></Button>
+          title={location} />
+        </MapView>
+      <View>
+        <TextInput placeholder="enter a location" style={styles.input} onChangeText={text => setLocation(location)}/>
+        <View style={{ width:Dimensions.get("window").width * 1.0, flexDirection: 'row', justifyContent: 'center', marginTop: 5}}>
+          <Button onPress={getLocation}title="Show"></Button>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: StatusBar.currentHeight,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
@@ -63,8 +60,10 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%"
   },
-  input : {
+  input: {
+    borderWidth: 1,
+    height: 40,
     
-    fontSize: 20,
-  },
+    justifyContent: 'center'
+  }
 });
